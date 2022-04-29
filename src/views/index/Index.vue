aeb6199790d963b3b3bb9d028df1747effb1f774
poicc <2631315464@qq.com>
2022-04-29 19:06:48 +0800

    style: 修改主题色

--- src/views/index/Index.vue
+++ src/views/index/Index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>首页</div>
+  <div>
+    <div>导航条</div>
+    <div class="wrapper">
+      <div class="wrapper__nav">
+        <div class="iconfont">&#xe727;</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +14,27 @@ export default {
   name: 'Index'
 }
 </script>
+
+<style lang="scss" scoped>
+@import '@/style/index.scss';
+.wrapper {
+  margin: 0 auto;
+  text-align: center;
+  width: 980px;
+  background: $bgColor;
+  &__nav {
+    display: flex;
+    .iconfont {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 40px;
+      height: 40px;
+      font-size: 30px;
+      border: 2px solid $primaryColor;
+      border-radius: 50%;
+      color: $primaryColor;
+    }
+  }
+}
+</style>
